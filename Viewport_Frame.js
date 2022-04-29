d1104a8d0d32f576402a7bdc266c5503bfbd2d4c
DEF <darth_ef@hotmail.com>
2022-04-29 18:19:41 +0800

    ts

---
+++ Viewport_Frame.js
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2022-04-29 09:56:57
+ * @LastEditors: Darth_Eternalfaith
+ * @LastEditTime: 2022-04-29 15:06:30
+ * @FilePath: \Editor\PrimitivesTGT-2D_Editor\js\import\CtrlLib__EXDEF_LIB\Viewport_Frame.js
+ */
+import {ExCtrl} from "../CtrlLib/CtrlLib.js"
+
+class Viewport_Frame extends ExCtrl{
+    constructor(viewport_tree){
+        this.viewport_tree=viewport_tree;
+    }
+}
