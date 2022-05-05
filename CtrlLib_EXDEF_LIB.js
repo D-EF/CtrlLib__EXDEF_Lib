4134d01676f2263b004ae1e1f4ef4f0c25727bcf
DEF <darth_ef@hotmail.com>
2022-05-05 17:41:49 +0800

    ts

--- CtrlLib_EXDEF_LIB.js
+++ CtrlLib_EXDEF_LIB.js
@@ -1,17 +1,27 @@
 /*
  * @Date: 2022-05-04 20:28:00
  * @LastEditors: Darth_Eternalfaith
- * @LastEditTime: 2022-05-04 21:34:18
+ * @LastEditTime: 2022-05-05 15:31:25
  * @FilePath: \Editor\PrimitivesTGT-2D_Editor\js\import\CtrlLib__EXDEF_LIB\CtrlLib_EXDEF_LIB.js
  */
-import { dependencyMapping, getCurrAbsPath, rltToAbs } from "../basics/Basics.js"
+import { add_DependencyListener, dependencyMapping, getCurrAbsPath, remove_DependencyListener_all, rltToAbs } from "../basics/Basics.js"
+import { DEF_VirtualElementList as VES, ExCtrl } from "../CtrlLib/CtrlLib.js";
 
 const JS_FILE_PATH = getCurrAbsPath();
 const XML_FILE_PATH = rltToAbs("./CtrlLib__EXDEF_LIB.xml",JS_FILE_PATH);
 
+
 var CtrlLib__EXDEF_LIB__XML_DATA={};
+/**
+ * @typedef type__CtrlLib__EXDEF_LIB__XML
+ * @property {Boolean} xml_loaded
+ * @property {VES} Viewport_Frame
+ */
+
+/** @type {type__CtrlLib__EXDEF_LIB__XML} */
 var CtrlLib__EXDEF_LIB__XML={};
 dependencyMapping(CtrlLib__EXDEF_LIB__XML,CtrlLib__EXDEF_LIB__XML_DATA,[
+    "xml_loaded",
     "Viewport_Frame"
 ]);
 
@@ -19,12 +29,31 @@ var xhr=new XMLHttpRequest();
 xhr.open("get",XML_FILE_PATH);
 xhr.onload=function(e){
     var str_sp=xhr.response.split("<ctrl_tab/>");
-    CtrlLib__EXDEF_LIB__XML.Viewport_Frame=str_sp[1];
+
+    CtrlLib__EXDEF_LIB__XML.Viewport_Frame=VES.xmlToVE(str_sp[1]);
+    
+    CtrlLib__EXDEF_LIB__XML.xml_loaded=true;
+    remove_DependencyListener_all(CtrlLib__EXDEF_LIB__XML,"xml_loaded");
 }
 xhr.send();
-
+class ExCtrl_DEF extends ExCtrl{
+    initialize(element){
+        if(CtrlLib__EXDEF_LIB__XML.xml_loaded){
+            return;
+        }
+        var that=this;
+        add_DependencyListener(CtrlLib__EXDEF_LIB__XML,"xml_loaded",function(xml_loaded){
+            console.log(xml_loaded)
+            if(xml_loaded){
+                that.addend(element);
+            }
+        })
+        return "stop";
+    }
+}
 export{
-    CtrlLib__EXDEF_LIB__XML_DL
+    CtrlLib__EXDEF_LIB__XML,
+    ExCtrl_DEF
 }
 
 
--- Viewport_Frame.js
+++ Viewport_Frame.js
@@ -1,18 +1,34 @@
 /*
  * @Date: 2022-04-29 09:56:57
  * @LastEditors: Darth_Eternalfaith
- * @LastEditTime: 2022-05-04 20:54:29
- * @FilePath: \Editor\PrimitivesTGT-2D_Editor\js\import\CtrlLib__EXDEF_LIB\Viewport_Frame.js
+ * @LastEditTime: 2022-05-05 17:20:12
+ * @FilePath: \PrimitivesTGT-2D_Editor\js\import\CtrlLib__EXDEF_LIB\Viewport_Frame.js
  */
-import {ExCtrl} from "../CtrlLib/CtrlLib.js"
-import {} from ""
-class Viewport_Frame extends ExCtrl{
-    constructor(viewport_tree){
-        this.viewport_tree=viewport_tree;
-    }
-    initialize(){
+import { dependencyMapping, Iterator__Tree } from "../basics/Basics.js";
+import { CtrlLib, DEF_VirtualElementList as VES } from "../CtrlLib/CtrlLib.js";
+import { CtrlLib__EXDEF_LIB__XML, ExCtrl_DEF } from "./CtrlLib_EXDEF_LIB.js";
 
+// 预设open
+    /**  */
+    var base={
+        p:true,
+        v:50,
+        child:[
+
+        ]
     }
+// 预设end 
+
+class Viewport_Frame extends ExCtrl_DEF{
+    constructor(viewport_tree){
+        super();
+        this.viewport_tree=viewport_tree||{};
+        this.iterator=new Iterator__Tree(this.viewport_tree);
+    }
+}
+dependencyMapping(Viewport_Frame.prototype,CtrlLib__EXDEF_LIB__XML,["bluePrint"],["Viewport_Frame"]);
+Viewport_Frame.prototype.childCtrlType={
+    null:CtrlLib
 }
 export {
     Viewport_Frame
